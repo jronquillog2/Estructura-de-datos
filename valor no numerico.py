686902ac2cf84572274861b56cd899a6da269447
Juan Ronquillo <85717668+jronquillog2@users.noreply.github.com>
2021-06-26 14:57:03 -0500

    Add files via upload

---
+++ valor no numerico.py
@@ -0,0 +1,13 @@
+nume=8
+nume= '16'
+if type(nume)==int:
+    nume= nume*2
+else:
+    print("El valor ingresado no es numerico")
+
+def mensaje(msg):
+    print(msg)
+
+
+mensaje("Gracias por regresar a Python")
+mensaje("Mi primer proyecto")
