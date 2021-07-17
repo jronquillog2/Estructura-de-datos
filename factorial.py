9c54c620cb603b134604aa05058b26083474f445
Juan Ronquillo <85717668+jronquillog2@users.noreply.github.com>
2021-07-17 18:48:55 -0500

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,17 @@
+class factorial:
+    def __init__(self):
+        n= int(input("Ingrese cantidad de numeros:")
+        for i in range(n):
+            num= int.input("Ingrese numero:")
+            #CALCULO DEL FACTORIAL
+            fac=1
+            #while num > 0
+            #    fac=fac*num
+             #   num= num -1
+
+            for num in range (num,0,-1)
+            #mientras num > 0
+                fac= fac*num
+
+            print("Factorial del numero:{} es:".format(num))
+
